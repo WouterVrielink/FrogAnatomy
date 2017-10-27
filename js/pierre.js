fad87b3378b28923ab5f2fad050645c34a332a7b
Wouter <wouter-vrielink@hotmail.com>
2017-10-27 12:36:59 +0200

    pierre cntre

--- js/pierre.js
+++ js/pierre.js
@@ -49,59 +49,54 @@ function init() {
   loader1.load( '../vtkaas/pierre_skeleton.vtk', function ( geometry ) {
 
     geometry.computeVertexNormals();
-    geometry.center();
-    geometry.computeBoundingBox();
-    beeybeey = geometry.boundingBox.min.clone();
-    // geometry.translate(beeybeey.x, beeybeey.y, beeybeey.z);
 
     var material = new THREE.MeshLambertMaterial( { color:0xf0f0f0, opacity:1, side: THREE.DoubleSide } );
     var mesh = new THREE.Mesh( geometry, material );
     meshesh["Skeleton"] = mesh;
 
     mesh.scale.multiplyScalar( scale );
-    // rotateObject( mesh, rot.x, rot.y , rot.z );
 
-    scene.add( mesh );
+    var pivot = new THREE.Group();
+    scene.add( pivot );
+    pivot.add( mesh );
 
-    load_vtk('../vtkaas/pierre_skin.vtk', scene, rot, scale, beeybeey, "#00ff00", 1, "Skin");
+    // load_vtk('../vtkaas/pierre_skin.vtk', scene, rot, scale, beeybeey, "#00ff00", .1, "Skin");
     // load_vtk('../vtkaas/pierre_skeleton.vtk', scene, rot, scale, beeybeey, "#f0f0f0", 1, "Skeleton");
-    load_vtk('../vtkaas/pierre_blood.vtk', scene, rot, scale, beeybeey, "red", 1, "Blood");
-    load_vtk('../vtkaas/pierre_brain.vtk', scene, rot, scale, beeybeey, "#a32030", 1, "Brain");
-    load_vtk('../vtkaas/pierre_duodenum.vtk', scene, rot, scale, beeybeey, "#998b58", 1, "Duodenum");
-    load_vtk('../vtkaas/pierre_eye_retina.vtk', scene, rot, scale, beeybeey, "#f4ee42", 1, "EyeRetina");
-    load_vtk('../vtkaas/pierre_eye_white.vtk', scene, rot, scale, beeybeey, "white", 1, "EyeWhite");
-    load_vtk('../vtkaas/pierre_heart.vtk', scene, rot, scale, beeybeey, "#ff3030", 1, "Heart");
-    load_vtk('../vtkaas/pierre_illeum.vtk', scene, rot, scale, beeybeey, "#8357a5", 1, "Ileum");
-    load_vtk('../vtkaas/pierre_kidney.vtk', scene, rot, scale, beeybeey, "#a51818", 1, "Kidney");
-    load_vtk('../vtkaas/pierre_l_intestine.vtk', scene, rot, scale, beeybeey, "#f9fc37", 1, "LargeIntestine");
-    load_vtk('../vtkaas/pierre_liver.vtk', scene, rot, scale, beeybeey, "#9e2525", 1, "Liver");
-    load_vtk('../vtkaas/pierre_lungs.vtk', scene, rot, scale, beeybeey, "#8482ff", 1, "Lungs");
-    load_vtk('../vtkaas/pierre_nerve.vtk', scene, rot, scale, beeybeey, "#feffef", 1, "Nerve");
-    load_vtk('../vtkaas/pierre_spleen.vtk', scene, rot, scale, beeybeey, "#efa5e9", 1, "Spleen");
-    load_vtk('../vtkaas/pierre_stomach.vtk', scene, rot, scale, beeybeey, "#ba9e23", 1, "Stomach");
+    load_vtk(pivot, '../vtkaas/pierre_blood.vtk', scene, rot, scale, beeybeey, "red", 1, "Blood");
+    load_vtk(pivot, '../vtkaas/pierre_brain.vtk', scene, rot, scale, beeybeey, "#a32030", 1, "Brain");
+    load_vtk(pivot, '../vtkaas/pierre_duodenum.vtk', scene, rot, scale, beeybeey, "#998b58", 1, "Duodenum");
+    load_vtk(pivot, '../vtkaas/pierre_eye_retina.vtk', scene, rot, scale, beeybeey, "#f4ee42", 1, "EyeRetina");
+    load_vtk(pivot, '../vtkaas/pierre_eye_white.vtk', scene, rot, scale, beeybeey, "white", 1, "EyeWhite");
+    load_vtk(pivot, '../vtkaas/pierre_heart.vtk', scene, rot, scale, beeybeey, "#ff3030", 1, "Heart");
+    load_vtk(pivot, '../vtkaas/pierre_kidney.vtk', scene, rot, scale, beeybeey, "#a51818", 1, "Kidney");
+    load_vtk(pivot, '../vtkaas/pierre_illeum.vtk', scene, rot, scale, beeybeey, "#8357a5", 1, "Ileum");
+    load_vtk(pivot, '../vtkaas/pierre_l_intestine.vtk', scene, rot, scale, beeybeey, "#f9fc37", 1, "LargeIntestine");
+    load_vtk(pivot, '../vtkaas/pierre_liver.vtk', scene, rot, scale, beeybeey, "#9e2525", 1, "Liver");
+    load_vtk(pivot, '../vtkaas/pierre_lungs.vtk', scene, rot, scale, beeybeey, "#8482ff", 1, "Lungs");
+    load_vtk(pivot, '../vtkaas/pierre_nerve.vtk', scene, rot, scale, beeybeey, "#feffef", 1, "Nerve");
+    load_vtk(pivot, '../vtkaas/pierre_spleen.vtk', scene, rot, scale, beeybeey, "#efa5e9", 1, "Spleen");
+    load_vtk(pivot, '../vtkaas/pierre_stomach.vtk', scene, rot, scale, beeybeey, "#ba9e23", 1, "Stomach");
 
+    pivot.position.set( -0.5, 1.75, -2 );
+    rotateObject( pivot, rot.x, rot.y, rot.z);
 
   });
 }
 
-function load_vtk(fileName, scene, rot, scale, beeybeey, color, opacity, index) {
+function load_vtk(pivot, fileName, scene, rot, scale, beeybeey, color, opacity, index) {
   var loader = new THREE.VTKLoader();
 
   loader.load( fileName, function ( geometry ) {
 
     geometry.computeVertexNormals();
-    geometry.computeBoundingBox()
-    geometry.translate(beeybeey.x, beeybeey.y, beeybeey.z);
 
     var material = new THREE.MeshLambertMaterial( { color: color, opacity: opacity, side: THREE.DoubleSide } );
     var mesh = new THREE.Mesh( geometry, material );
     meshesh[index] = mesh;
 
-    mesh.position.set( 0, 0, 0 );
     mesh.scale.multiplyScalar( scale );
-    // rotateObject( mesh, rot.x, rot.y, rot.z );
 
-    scene.add( mesh );
+    pivot.add( mesh );
   });
 
   return beeybeey ;
